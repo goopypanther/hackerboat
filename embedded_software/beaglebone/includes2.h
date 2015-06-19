548538edca22cc877f9edaec013c5248a91d6f6b
Jeremy Ruhland (teller) <jeremy@goopypanther.org>
2015-06-19 03:24:18 -0700

    new main includes file

---
+++ embedded_software/beaglebone/includes2.h
@@ -0,0 +1,28 @@
+/**
+ * @file includes2.h
+ * @brief
+ *
+ * @author Jeremy Ruhland <jeremy ( a t ) goopypanther.org>
+ *
+ * @version
+ * @since Jun 18, 2015
+ */
+
+#ifndef INCLUDES2_H_
+#define INCLUDES2_H_
+
+#include <mavlink.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "udp.h"
+#include "log.h"
+#include "args.h"
+#include "mavlinkWrapper.h"
+
+#endif /* INCLUDES2_H_ */
