8cafa53cd5d21372087c5ed08ab566805d1e88bb
Pierce Nichols <pierce@logos-electro.com>
2015-09-06 20:50:50 -0700

    added calibration constants

--- embedded_software/arduino/HackerbotPilot/HackerbotPilot.ino
+++ embedded_software/arduino/HackerbotPilot/HackerbotPilot.ino
@@ -18,18 +18,18 @@
 
 // calibration constants
 // these constants are used to calibrate the accelerometer and magnetometer
-#define X_ACCEL_OFFSET          (0)
-#define X_ACCEL_GAIN            (1)
-#define Y_ACCEL_OFFSET          (0)
-#define Y_ACCEL_GAIN            (1)
-#define Z_ACCEL_OFFSET          (0)
-#define Z_ACCEL_GAIN            (1)
-#define X_MAG_OFFSET            (0)
-#define X_MAG_GAIN              (1)
+#define X_ACCEL_OFFSET          (-0.275)
+#define X_ACCEL_GAIN            (0.092378753)
+#define Y_ACCEL_OFFSET          (2.825)
+#define Y_ACCEL_GAIN            (0.07160759)
+#define Z_ACCEL_OFFSET          (-3.925)
+#define Z_ACCEL_GAIN            (0.06655574)
+#define X_MAG_OFFSET            (-24.725)
+#define X_MAG_GAIN              (0.018003421)
 #define Y_MAG_OFFSET            (0)
-#define Y_MAG_GAIN              (1)
-#define Z_MAG_OFFSET            (0)
-#define Z_MAG_GAIN              (1)
+#define Y_MAG_GAIN              (0.017571604)
+#define Z_MAG_OFFSET            (-5.92)
+#define Z_MAG_GAIN              (0.017253278)
 
 
 // test limits
