9433160e4446a46f5000202adf20e318a0af46dc
Pierce Nichols <pierce@logos-electro.com>
2015-11-25 22:58:07 -0800

    Starting the refactor...

---
+++ embedded_software/arduino/HackerboatPilot/sensorCalibration.h
@@ -0,0 +1,14 @@
+// calibration constants
+// these constants are used to calibrate the accelerometer and magnetometer
+#define X_ACCEL_OFFSET          (0)
+#define X_ACCEL_GAIN            (0.1)
+#define Y_ACCEL_OFFSET          (0)
+#define Y_ACCEL_GAIN            (0.1)
+#define Z_ACCEL_OFFSET          (0)
+#define Z_ACCEL_GAIN            (0.1)
+#define X_MAG_OFFSET            (-39.59)
+#define X_MAG_GAIN              (0.933)
+#define Y_MAG_OFFSET            (10.82)
+#define Y_MAG_GAIN              (0.943)
+#define Z_MAG_OFFSET            (-23.16)
+#define Z_MAG_GAIN              (1.054)
