0ec6bf3afa92be4787798f24a9c120504e8fb69c
Pierce Nichols <pierce@logos-electro.com>
2016-01-24 18:48:56 -0800

    Rearranging files...

--- embedded_software/beaglebone/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#define _POSIX_SOURCE
-#define __USE_XOPEN2KXSI
-#define __USE_XOPEN
-
-gcc -O2 -I"./include/" -D_POSIX_SOURCE -D__USE_XOPEN2KXSI -D__USE_XOPEN -Wall -std=c99 stalkerLink.c -o stalkerLink
-
