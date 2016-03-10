c4134fb408b9869e2f96f1fa1f8813ac8ce7841a
Wim Lewis <wiml@hhhh.org>
2016-03-10 23:41:11 +0000

    Splitting the boneState and arduinoState class declarations into their own headers. This is needed to break a circular include dependency now that the GPS class is in its own file as well.

---
+++ embedded_software/beaglebone/include/arduinoState.hpp
@@ -0,0 +1,133 @@
+/******************************************************************************
+ * Hackerboat Beaglebone types module
+ * arduinoState.hpp
+ * see the Hackerboat documentation for more details
+ * Written by Pierce Nichols, Jan 2016
+ * 
+ * Version 0.1: First alpha
+ *
+ ******************************************************************************/
+
+#ifndef ARDUINOSTATE_H
+#define ARDUINOSTATE_H
+
+#include <time.h>
+#include <string>
+#include "stateStructTypes.hpp" 
+#include "location.hpp"
+
+/**
+ * @class arduinoStateClass
+ *
+ * @brief Class for storing the current state of the Arduino element
+ *
+ */
+
+class arduinoStateClass : public hackerboatStateClassStorable {
+	public:
+	
+		/**
+		 * @brief An enum to store the current state of the Arduino.
+		 */
+		enum arduinoStateEnum {
+			BOAT_POWERUP     	= 0,  		/**< The boat enters this state at the end of initialization */
+			BOAT_ARMED			= 1,  		/**< In this state, the boat is ready to receive go commands over RF */
+			BOAT_SELFTEST   	= 2,  		/**< After powerup, the boat enters this state to determine whether it's fit to run */
+			BOAT_DISARMED   	= 3,  		/**< This is the default safe state. No external command can start the motor */
+			BOAT_ACTIVE     	= 4,  		/**< This is the normal steering state */
+			BOAT_LOWBATTERY   	= 5,  		/**< The battery voltage has fallen below that required to operate the motor */
+			BOAT_FAULT    		= 6,  		/**< The boat is faulted in some fashion */
+			BOAT_SELFRECOVERY 	= 7,   		/**< The Beaglebone has failed and/or is not transmitting, so time to self-recover*/
+			BOAT_ARMEDTEST		= 8,		/**< The Arduino is accepting specific pin read/write requests for hardware testing. */
+			BOAT_ACTIVERUDDER	= 9,		/**< The Arduino is accepting direct rudder commands */
+			BOAT_NONE			= 10		/**< Provides a null value for no command yet received */
+		};        
+
+		arduinoStateClass (void);
+		arduinoStateClass (const char *file, size_t len);
+		
+		bool populate (void);	/**< Populate the object from the named interface */
+		bool setCommand (arduinoStateEnum c);
+		
+		// Command functions...
+		bool writeBoneState(boneStateClass::boneStateEnum s);
+		bool writeCommand(void);
+		int16_t writeThrottle(void);
+		double writeHeadingTarget(void);
+		double writeHeadingDelta(double delta);
+		bool heartbeat(void);
+		
+		bool 				popStatus;				/**< State of whether the last call to populate() succeeded or failed */
+		timespec			uTime;					/**< Time the record was made */
+		arduinoStateEnum 	state;					/**< The current state of the boat                    */
+		arduinoStateEnum	command;				/**< Last state command received by the Arduino */
+		int8_t		 		throttle;   			/**< The current throttle position                    */
+		boneStateClass::boneStateEnum 	bone;		/**< The current state of the BeagleBone                */
+		orientationClass	orientation;			/**< The current accelerometer tilt and magnetic heading of the boat  */
+		float 				headingTarget;			/**< The desired magnetic heading                     */  
+		float 				internalVoltage;		/**< The battery voltage measured on the control PCB          */
+		float 				batteryVoltage;			/**< The battery voltage measured at the battery            */
+		float				motorVoltage;
+		bool				enbButton;				/**< State of the enable button. off = 0; on = 0xff           */
+		bool				stopButton;				/**< State of the emergency stop button. off = 0; on = 0xff       */
+		long 				timeSinceLastPacket;	/**< Number of milliseconds since the last command packet received    */
+		long 				timeOfLastPacket;		/**< Time the last packet arrived */
+		long 				timeOfLastBoneHB;	
+		long 				timeOfLastShoreHB;
+		string				stateString;
+		string 				boneStateString;
+		string				commandString;
+		uint16_t			faultString;			/**< Fault string -- binary string to indicate source of faults */
+		float 				rudder;
+		int16_t				rudderRaw;
+		int16_t				internalVoltageRaw;
+		int16_t				motorVoltageRaw;
+		float				motorCurrent;
+		int16_t				motorCurrentRaw;
+		float				Kp;
+		float				Ki;
+		float				Kd;
+		float	 			magX;
+		float 				magY;
+		float 				magZ;
+		float 				accX;
+		float 				accY;
+		float 				accZ;
+		float 				gyroX;
+		float 				gyroY;
+		float 				gyroZ;
+		bool	 			horn;
+		bool				motorDirRly;
+		bool				motorWhtRly;
+		bool				motorYlwRly;
+		bool				motorRedRly;
+		bool				motorRedWhtRly;
+		bool				motorRedYlwRly;
+		bool				servoPower;
+		long 				startStopTime;
+		long				startStateTime;
+		arduinoStateEnum	originState;
+		
+		static const string const arduinoStates[] = {
+			"PowerUp", 
+			"Armed", 
+			"SelfTest", 
+			"Disarmed", 
+			"Active", 
+			"LowBattery", 
+			"Fault", 
+			"SelfRecovery", 
+			"ArmedTest", 
+			"ActiveRudder", 
+			"None"
+		};
+		static const uint8_t 	arduinoStateCount = 11;
+		
+	private:
+		bool 					setState (arduinoStateEnum s);
+		bool 					setBoneState (boneStateClass::boneStateEnum s);
+		string					write(string func, string query);		/**< Write to a function on the Arduino */
+		
+};
+
+#endif /* ARDUINOSTATE_H */
---
+++ embedded_software/beaglebone/include/boneState.hpp
@@ -0,0 +1,95 @@
+/******************************************************************************
+ * Hackerboat Beaglebone types module
+ * boneState.hpp
+ * see the Hackerboat documentation for more details
+ * Written by Pierce Nichols, Jan 2016
+ * 
+ * Version 0.1: First alpha
+ *
+ ******************************************************************************/
+
+#ifndef BONESTATESTRUCT_H
+#define BONESTATESTRUCT_H
+
+#include <time.h>
+#include <string>
+#include "stateStructTypes.hpp" 
+#include "location.hpp"
+#include "gps.hpp"
+
+
+/**
+ * @class boneStateClass
+ *
+ * @brief Class for storing the current state of the Beaglebone element
+ *
+ */
+
+class boneStateClass : public hackerboatStateClassStorable {
+	public:
+		/**
+		 * @brief Beaglebone state
+		 */
+		enum boneStateEnum {
+			BONE_START			= 0,  		/**< Initial starting state         */
+			BONE_SELFTEST		= 1,  		/**< Initial self-test            */
+			BONE_DISARMED		= 2,  		/**< Disarmed wait state          */  
+			BONE_FAULT			= 3,		/**< Beaglebone faulted           */ 
+			BONE_ARMED			= 4,		/**< Beaglebone armed & ready to navigate */ 
+			BONE_MANUAL			= 5,		/**< Beaglebone manual steering       */ 
+			BONE_WAYPOINT		= 6,		/**< Beaglebone navigating by waypoints   */
+			BONE_NOSIGNAL		= 7,		/**< Beaglebone has lost shore signal    */
+			BONE_RETURN			= 8,		/**< Beaglebone is attempting to return to start point */
+			BONE_ARMEDTEST		= 9,		/**< Beaglebone accepts all commands that would be valid in any unsafe state */
+			BONE_NONE			= 10		/**< State of the Beaglebone is currently unknown	*/
+		};
+	
+		bool insertFault (const string fault);	/**< Add the named fault to the fault string. Returns false if fault string is full */
+		bool removeFault (const string fault);	/**< Remove the named fault to the fault string. Returns false if not present */
+		bool hasFault (const string fault);		/**< Returns true if given fault is present */
+		int faultCount (void);					/**< Returns the current number of faults */
+		bool setState (boneStateEnum s);		/**< Set state to the given value */
+		bool setCommand (boneStateEnum c);		/**< Set command to the given value */
+		bool setArduinoState (arduinoStateClass::arduinoStateEnum s); /**< Set Arduino state to the given value */
+		
+		timespec 					uTime;				/**< Time the record was made */
+		timespec					lastContact;		/**< Time of the last contact from the shore station */
+		boneStateEnum				state = BONE_NONE;	/**< current state of the beaglebone */	
+		string						stateString;		/**< current state of the beaglebone, human readable string */
+		boneStateEnum				command = BONE_NONE;/**< commanded state of the beaglebone */
+		string						commandString;		/**< commanded state of the beaglebone, human readable string */
+		arduinoStateClass::arduinoStateEnum			ardState;		/**< current state of the Arduino */
+		string						ardStateString;		/**< current state of the Arduino, human readable string */
+		string						faultString;		/**< comma separated list of faults */
+		gpsFixClass					gps;				/**< current GPS position */
+		int32_t						waypointNext;		/**< ID of the current target waypoint */
+		double						waypointStrength;	/**< Strength of the waypoint */
+		double						waypointAccuracy;	/**< How close the boat gets to each waypoint before going to the next one */
+		double						waypointStrengthMax;/**< Maximum waypoint strength */
+		bool						autonomous;			/**< When set true, the boat will operate autonomously */	
+		locationClass				launchPoint;		/**< Location from which the boat departed */
+		
+		static const string boneStates[] = {
+			"Start", 
+			"SelfTest", 
+			"Disarmed", 
+			"Fault",
+			"Armed", 
+			"Manual", 
+			"WaypointNavigation",
+			"LossOfSignal", 
+			"ReturnToLaunch", 
+			"ArmedTest",
+			"None"
+		};		
+		static const uint8_t boneStateCount = 11;
+		
+	private:
+		void initHashes (void);								/**< Initialize state name hashes */
+		static const char *_format = "{s:o,s:o,s:i,s:s,s:i,s:s,s:i,s:s,s:s,s:o,s:i,s:f,s:f,s:f,s:b,s:o}";
+		static uint32_t stateHashes[boneStateCount];		/**< All the state names, hashed for easy lookup */
+		
+
+};
+
+#endif /* BONESTATESTRUCT_H */
