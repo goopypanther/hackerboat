ee4140befeb6b543ba360afe2ba0c77b6cab092d
Pierce Nichols <pierce@logos-electro.com>
2016-04-10 22:22:10 -0700

    Found the problem with frame time calculations
    Tinkering with the time calculations...

--- embedded_software/beaglebone/include/timer_utils.hpp
+++ embedded_software/beaglebone/include/timer_utils.hpp
@@ -15,20 +15,20 @@
 #include <signal.h>
 #include <time.h>
 
+#define BILLION (1000000000)
+
 void norm_timespec (timespec* a) {
-	while (a->tv_nsec >= 1e9) {
+	while (a->tv_nsec >= BILLION) {
 		a->tv_sec++;
-		a->tv_nsec -= 1e9;
+		a->tv_nsec -= BILLION;
 	}
 	while (a->tv_nsec < 0) {
 		a->tv_sec--;
-		a->tv_nsec += 1e9;
+		a->tv_nsec += BILLION;
 	}
 }
 
 void add_timespec (timespec* a, timespec* b, timespec* result) {
-	int64_t ns_sum; 
-	
 	result->tv_sec = a->tv_sec + b->tv_sec;
 	result->tv_nsec = a->tv_nsec + b->tv_nsec;
 	norm_timespec(result);
@@ -38,8 +38,8 @@ bool subtract_timespec (timespec* a, timespec* b, timespec* result) {
 	if (b->tv_sec > a->tv_sec) return false;	// This would result in a negative timespec, which is clearly forbidden by spec
 	if ((b->tv_sec == a->tv_sec) &&				// This would also result in a negative timespec
 		(b->tv_nsec > a->tv_nsec)) return false;
-	result->tv_sec = a->tv_sec - b->tv_sec;
-	result->tv_nsec = a->tv_sec - b->tv_nsec;
+	result->tv_sec = ((a->tv_sec) - (b->tv_sec));
+	result->tv_nsec = ((a->tv_nsec) - (b->tv_nsec));
 	norm_timespec(result);
 	
 	return true;
