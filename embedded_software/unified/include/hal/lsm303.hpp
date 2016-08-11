137d42ba295fdc1e238525542421042d33f5e324
Pierce Nichols <pierce@logos-electro.com>
2016-08-11 08:57:25 -0700

    Added LSM303 header

---
+++ embedded_software/unified/include/hal/lsm303.hpp
@@ -0,0 +1,151 @@
+/******************************************************************************
+ * Hackerboat Beaglebone LSM303 module
+ * hal/lsm303.hpp
+ * This module provides an interface to the LSM303 accelerometer & compass 
+ * see the Hackerboat documentation for more details
+ * Code is derived from the Adafruit LSM303 and Adafruit Sensor libraries
+ * Written by Pierce Nichols, Aug 2016
+ * 
+ * Version 0.1: First alpha
+ *
+ ******************************************************************************/
+
+#ifndef LSM303_H
+#define LSM303_H
+
+#include <errno.h>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <time.h>
+#include "hal/config.h"
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <map>
+
+
+
+/*=========================================================================
+    I2C ADDRESS/BITS
+    -----------------------------------------------------------------------*/
+    #define LSM303_ADDRESS_ACCEL          (0x32 >> 1)         // 0011001x
+    #define LSM303_ADDRESS_MAG            (0x3C >> 1)         // 0011110x
+/*=========================================================================*/
+
+/*=========================================================================
+    REGISTERS
+    -----------------------------------------------------------------------*/
+    enum class lsm303AccelRegisters_t
+    {                                                     // DEFAULT    TYPE
+      LSM303_REGISTER_ACCEL_CTRL_REG1_A         = 0x20,   // 00000111   rw
+      LSM303_REGISTER_ACCEL_CTRL_REG2_A         = 0x21,   // 00000000   rw
+      LSM303_REGISTER_ACCEL_CTRL_REG3_A         = 0x22,   // 00000000   rw
+      LSM303_REGISTER_ACCEL_CTRL_REG4_A         = 0x23,   // 00000000   rw
+      LSM303_REGISTER_ACCEL_CTRL_REG5_A         = 0x24,   // 00000000   rw
+      LSM303_REGISTER_ACCEL_CTRL_REG6_A         = 0x25,   // 00000000   rw
+      LSM303_REGISTER_ACCEL_REFERENCE_A         = 0x26,   // 00000000   r
+      LSM303_REGISTER_ACCEL_STATUS_REG_A        = 0x27,   // 00000000   r
+      LSM303_REGISTER_ACCEL_OUT_X_L_A           = 0x28,
+      LSM303_REGISTER_ACCEL_OUT_X_H_A           = 0x29,
+      LSM303_REGISTER_ACCEL_OUT_Y_L_A           = 0x2A,
+      LSM303_REGISTER_ACCEL_OUT_Y_H_A           = 0x2B,
+      LSM303_REGISTER_ACCEL_OUT_Z_L_A           = 0x2C,
+      LSM303_REGISTER_ACCEL_OUT_Z_H_A           = 0x2D,
+      LSM303_REGISTER_ACCEL_FIFO_CTRL_REG_A     = 0x2E,
+      LSM303_REGISTER_ACCEL_FIFO_SRC_REG_A      = 0x2F,
+      LSM303_REGISTER_ACCEL_INT1_CFG_A          = 0x30,
+      LSM303_REGISTER_ACCEL_INT1_SOURCE_A       = 0x31,
+      LSM303_REGISTER_ACCEL_INT1_THS_A          = 0x32,
+      LSM303_REGISTER_ACCEL_INT1_DURATION_A     = 0x33,
+      LSM303_REGISTER_ACCEL_INT2_CFG_A          = 0x34,
+      LSM303_REGISTER_ACCEL_INT2_SOURCE_A       = 0x35,
+      LSM303_REGISTER_ACCEL_INT2_THS_A          = 0x36,
+      LSM303_REGISTER_ACCEL_INT2_DURATION_A     = 0x37,
+      LSM303_REGISTER_ACCEL_CLICK_CFG_A         = 0x38,
+      LSM303_REGISTER_ACCEL_CLICK_SRC_A         = 0x39,
+      LSM303_REGISTER_ACCEL_CLICK_THS_A         = 0x3A,
+      LSM303_REGISTER_ACCEL_TIME_LIMIT_A        = 0x3B,
+      LSM303_REGISTER_ACCEL_TIME_LATENCY_A      = 0x3C,
+      LSM303_REGISTER_ACCEL_TIME_WINDOW_A       = 0x3D
+    };
+    
+    enum class lsm303MagRegisters_t
+    {
+      LSM303_REGISTER_MAG_CRA_REG_M             = 0x00,
+      LSM303_REGISTER_MAG_CRB_REG_M             = 0x01,
+      LSM303_REGISTER_MAG_MR_REG_M              = 0x02,
+      LSM303_REGISTER_MAG_OUT_X_H_M             = 0x03,
+      LSM303_REGISTER_MAG_OUT_X_L_M             = 0x04,
+      LSM303_REGISTER_MAG_OUT_Z_H_M             = 0x05,
+      LSM303_REGISTER_MAG_OUT_Z_L_M             = 0x06,
+      LSM303_REGISTER_MAG_OUT_Y_H_M             = 0x07,
+      LSM303_REGISTER_MAG_OUT_Y_L_M             = 0x08,
+      LSM303_REGISTER_MAG_SR_REG_Mg             = 0x09,
+      LSM303_REGISTER_MAG_IRA_REG_M             = 0x0A,
+      LSM303_REGISTER_MAG_IRB_REG_M             = 0x0B,
+      LSM303_REGISTER_MAG_IRC_REG_M             = 0x0C,
+      LSM303_REGISTER_MAG_TEMP_OUT_H_M          = 0x31,
+      LSM303_REGISTER_MAG_TEMP_OUT_L_M          = 0x32
+    };
+/*=========================================================================*/
+
+/*=========================================================================
+    MAGNETOMETER GAIN SETTINGS
+    -----------------------------------------------------------------------*/
+    enum class lsm303MagGain
+    {
+      LSM303_MAGGAIN_1_3                        = 0x20,  // +/- 1.3
+      LSM303_MAGGAIN_1_9                        = 0x40,  // +/- 1.9
+      LSM303_MAGGAIN_2_5                        = 0x60,  // +/- 2.5
+      LSM303_MAGGAIN_4_0                        = 0x80,  // +/- 4.0
+      LSM303_MAGGAIN_4_7                        = 0xA0,  // +/- 4.7
+      LSM303_MAGGAIN_5_6                        = 0xC0,  // +/- 5.6
+      LSM303_MAGGAIN_8_1                        = 0xE0   // +/- 8.1
+    };	
+/*=========================================================================*/
+
+/*=========================================================================
+    CHIP ID
+    -----------------------------------------------------------------------*/
+    #define LSM303_ID                     (0b11010100)
+/*=========================================================================*/
+
+class lsm303 {
+	public:
+		lsm303(std::string devpath);
+
+		bool begin(void);
+		bool readAll (void);
+		void enableAutoRange(bool enable);
+		void setMagGain(lsm303MagGain gain);
+		map<std::string, double> getMagData (void);
+		map<std::string, double> getAccelData (void);
+		map<std::string, int> getRawMagData (void);
+		map<std::string, int> getRawAccelData (void);
+		void setMagRegister(lsm303MagRegisters_t reg, uint8_t val);	/**< Set an arbitrary register on the chip. */
+		uint8_t getMagRegister(lsm303MagRegisters_t reg);			/**< Read an arbitrary register on the chip. */
+		void setAccelRegister(lsm303AccelRegisters_t reg, uint8_t val);	/**< Set an arbitrary register on the chip. */
+		uint8_t getAccelRegister(lsm303AccelRegisters_t reg);			/**< Read an arbitrary register on the chip. */
+		map<std::string, int> getMagOffset (void);
+		map<std::string, int> getAccelOffset (void);
+		map<std::string, double> getMagScale (void);
+		map<std::string, double> getAccelScale (void);
+		void setMagOffset (map<std::string, int>);
+		void setAccelOffset (map<std::string, int>);
+		void setMagScale (map<std::string, double>);
+		void setAccelScale (map<std::string, double>);
+
+	private:
+		map<std::string, int> _accelData;   
+		map<std::string, int>	_magData;
+		map<std::string, int>   _magGain;
+
+};
+
+
+#endif
