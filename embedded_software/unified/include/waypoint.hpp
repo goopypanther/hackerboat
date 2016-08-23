31b3d7bc614c1201f03062d530b3a50d30dc1adc
Pierce Nichols <pierce@logos-electro.com>
2016-08-23 00:09:21 +0000

    Adding waypoint.hpp

---
+++ embedded_software/unified/include/waypoint.hpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+ * Hackerboat waypoint module
+ * waypoint.hpp
+ * This module parses the kml file that defines the waypoints and presents them.
+ * Much code copied shamelessly from the libkml examples, particularly 
+ * printgeometry.cc
+ *
+ * See the Hackerboat documentation for more details
+ * Written by Pierce Nichols, Aug 2016
+ * 
+ * Version 0.1: First alpha
+ *
+ ******************************************************************************/
+ 
+#ifndef DODGE_H
+#define DODGE_H
+
+#include <chrono>
+#include <vector>
+#include <tuple>
+#include <map>
+#include <iostream>
+#include <string>
+#include "kml/dom.h"
+#include "kml/base/file.h"
+#include "logs.hpp"
+#include "location.hpp"
+#include "hackerboatRoot.hpp"
+#include "hal/config.h"
+
