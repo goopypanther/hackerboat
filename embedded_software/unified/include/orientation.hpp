c06a143216d0a05b815ad693404d8a260583c0b4
Pierce Nichols <pierce@logos-electro.com>
2016-09-15 07:05:31 +0000

    Added headingError function to Orientation Added declination awareness to Orientation Updated MANIFEST to reflect recent changes

--- embedded_software/unified/include/orientation.hpp
+++ embedded_software/unified/include/orientation.hpp
@@ -17,6 +17,9 @@
 #include <cmath>
 #include <string>
 #include "hackerboatRoot.hpp"
+#include "location.hpp"
+#include <GeographicLib/MagneticModel.hpp>
+#include <GeographicLib/Geocentric.hpp>
 
 /**
  * @class Orientation
@@ -34,18 +37,26 @@ class Orientation : public HackerboatState {
 		json_t *pack () const;					/**< Create a json object of this orientation */
 		bool isValid ();						/**< Check if this is a valid orientation object */
 		bool normalize (void);					/**< Normalize the roll/pitch/heading values to +/-180 degrees (or 0-360 degrees in the case of heading) */
+		double headingError (double target);	/**< Get the error angle between this Orientation and target heading, in degrees. */	
+		Orientation makeTrue ();				/**< Return an Orientation object with the heading as a true (rather than magnetic) heading. Requires location to compute magnetic declination. */
+		Orientation makeMag ();					/**< Return an Orientation object with the heading as a magnetic (rather than true) heading. Requires location to compute magnetic declination. */
+		bool isMagnetic() {return magnetic;};	/**< Returns true if the heading is magnetic rather than true */ 
+		bool updateDeclination(Location loc);
 		double roll 	= NAN;			
 		double pitch 	= NAN;
 		double heading 	= NAN;
 
 	private:
 		double normAxis (double val, const double max, const double min) const;		/**< Normalize given axis */
+		bool magnetic = true;
+		static double declination;
 		static const double constexpr	maxRoll = 180.0;
 		static const double constexpr	minRoll = -180.0;
 		static const double constexpr	maxPitch = 180.0;
 		static const double constexpr	minPitch = -180.0;
 		static const double constexpr	maxHeading = 360.0;
 		static const double constexpr	minHeading = 0.0;
+	
 };
 
 #endif
--- embedded_software/unified/src/common/orientation.cpp
+++ embedded_software/unified/src/common/orientation.cpp
@@ -13,7 +13,11 @@
 #include <cstdlib>
 #include <string>
 #include <jansson.h>
+#include <chrono>
+#include <ctime>
 #include "orientation.hpp"
+#include <GeographicLib/MagneticModel.hpp>
+#include <GeographicLib/Geocentric.hpp>
 
 #define GET_VAR(var) ::parse(json_object_get(input, #var), &var)
 
@@ -53,4 +57,47 @@ double Orientation::normAxis (double val, const double max, const double min) co
 	while (val < min) val += (max - min);
 	if (val > max) return fmod(val, max);
 	return val;
+}
+
+double Orientation::headingError (double target) {
+	if (!isValid()) return NAN;
+	return normAxis((heading - target), maxHeading, minHeading);
+}
+
+Orientation Orientation::makeTrue () {
+	if (!this->isMagnetic()) return *this;
+	Orientation output = *this;
+	output.heading += declination;
+	output.normalize();
+	return output;
+}
+
+Orientation Orientation::makeMag () {
+	if (this->isMagnetic()) return *this;
+	Orientation output = *this;
+	output.heading -= declination;
+	output.normalize();
+	return output;
+}
+
+bool Orientation::updateDeclination (Location loc) {
+	if (!loc.isValid()) return false;
+	// time information for the mag model
+	sysclock thisTime = std::chrono::system_clock::now();
+	time_t tt = std::chrono::system_clock::to_time_t(thisTime);
+	tm utc_tm = *gmtime(&tt);
+	// mag model
+	GeographicLib::MagneticModel mag { "emm2015" };
+	// intermediate values
+	double Bx, By, Bz, H;
+	// output values
+	double strength, inclination;
+	
+	// Grab the magnetic components
+	mag(utc_tm.tm_year + 1900, loc.lat, loc.lon, 0, Bx, By, Bz);
+	
+	// convert intermediate representation to the output
+	GeographicLib::MagneticModel::FieldComponents(Bx, By, Bz, H, strength, declination, inclination);
+	
+	return true;
 }
